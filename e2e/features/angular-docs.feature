3a6c5709735050ea5517d758583530e778f7d371
sakalmykov <s.kalmykov@domclick.ru>
2018-05-22 12:07:09 +0300

    edit Readme

--- e2e/features/angular-docs.feature
+++ e2e/features/angular-docs.feature
@@ -2,6 +2,7 @@
 
 Feature: Check angular docs
 
+    @Smoke
     Scenario: Docs page should just work
         When I'm on angular-docs page
         Then I see the logo
---
+++ e2e/protractor-smoke.conf.js
@@ -0,0 +1,27 @@
+// Protractor configuration file, see link for more information
+// https://github.com/angular/protractor/blob/master/lib/config.ts
+
+exports.config = {
+    allScriptsTimeout: 30 * 1000,
+    framework: 'custom',
+    frameworkPath: require.resolve('protractor-cucumber-framework'),
+    specs: [
+        './features/*.feature'
+    ],
+    cucumberOpts: {
+        compiler: "ts:ts-node/register",
+        require: './features/step_definitions/*.steps.ts',
+        tags: '~@Ignore and @Smoke'
+    },
+    capabilities: {
+        browserName: 'chrome',
+        chromeOptions: {
+            args: [
+                "--headless", "--disable-gpu", "--window-size=1920,1080"
+            ]
+        }
+    },
+    directConnect: true,
+    baseUrl: 'https://qa.your-site.com',
+    ignoreUncaughtExceptions: true
+};
